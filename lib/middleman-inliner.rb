6aa1d9301a049d06355df0706467cfd263c3afc8
Gian-Reto Tarnutzer <gian@Gian-Retos-MacBook-Pro.local>
2017-07-12 17:25:11 +0200

    Fixing errors

--- lib/middleman-inliner.rb
+++ lib/middleman-inliner.rb
@@ -4,9 +4,9 @@ class Inliner < Middleman::Extension
   def initialize(app, options_hash={}, &block)
     super
 
-    app.compass_config do |config|
-      config.output_style = :compressed
-    end
+    # app.compass_config do |config|
+    #   config.output_style = :compressed
+    # end
   end
 
   helpers do
@@ -40,4 +40,5 @@ class Inliner < Middleman::Extension
   end
 end
 
-Inliner.register(:inliner)
+# Inliner.register(:inliner)
+::Middleman::Extensions.register(:inliner, Inliner)
